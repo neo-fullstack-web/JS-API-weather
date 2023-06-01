e12aee4d318efdb701ec2dc50d213e4a028a215f
Martin Rodriguez <neomartinr@gmail.com>
2023-05-31 21:31:48 -0300

    Promise introduction

---
+++ js/promises.js
@@ -0,0 +1,39 @@
+console.time('delayedName')
+let products;
+// let lateName;
+function esperarResultado() {
+
+    return new Promise(function(resolve, reject) {
+
+        setTimeout(() => {
+            products = ['XBOX', 'PS5', 'Nintendo', 'Family'];
+            // lateName = 'Delayed user';
+            resolve()
+        }, 4000);
+        
+
+    });
+
+}
+
+esperarResultado()
+    .then(() => {
+        console.log(`Se resolvió`);
+        products.forEach(prod => console.log(prod));
+
+    }).catch((error) => {
+        console.warn('La promesa falló')
+    })
+// esperarResultado()
+//     .then((resolucion) => {
+//         console.log(`La promesa se resolvió: ${lateName}`)
+//     })
+//     .catch(error => console.warn(error))
+
+// console.log();
+// console.log(lateName)
+
+
+console.log(`Una respuesta desde mi script`)
+console.log(`Una respuesta para ${name}`)
+
