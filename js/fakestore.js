49cf40a4450cf67f6480cffb3b9828b1464ac7ad
Martin Rodriguez <neomartinr@gmail.com>
2023-06-05 22:48:03 -0300

    Search with button, card styles

--- js/fakestore.js
+++ js/fakestore.js
@@ -19,16 +19,20 @@ function pintarProducts() {
 
     products.forEach(prod => {
         const card = document.createElement('div');
-        card.classList.add('card', 'col-4');
+        card.classList.add('col-4');
         card.innerHTML = `
-        <img src="${prod.image}" class="card-img-top" alt="${prod.title} image">
-        <div class="card-body">
-          <h5 class="card-title">${prod.title}</h5>
-          <p class="card-text">${prod.description}</p>
-          <div>
-            <small class="text-primary">${prod.category}</small>
+        <div class="card">
+          <img src="${prod.image}" class="card-img-top" alt="${prod.title} image">
+          <div class="card-body">
+            <h5 class="card-title">${prod.title}</h5>
+            <p class="card-text" title="${prod.description}">${prod.description}</p>
+            <div>
+              <small class="text-primary">${prod.category}</small>
+            </div>
+            
           </div>
-          <a href="/pages/product-detail/product-detail.html?id=${prod.id}&var2=Unvalor&price=dolar" class="btn btn-primary">Ver más</a>
+          <footer class="card-footer" >
+          <a href="/pages/product-detail/product-detail.html?id=${prod.id}&var2=Unvalor&price=dolar" class="btn btn-primary">Ver más</a></footer>
         </div>
         `
         container.appendChild(card)
--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,28 @@
 const apiKey = '862c70e380cf5f9e19a3425c76609dbb';
-const latitude = '-34.629060';
-const longitude =  '-68.338872';
-const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+// let latitude = '-34.629060';
+// let longitude =  '-68.338872';
+const URL = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric`;
+const weatherInput = document.getElementById('weather-search');
 
+// searchWeatherCity();
 
-fetch(URL).then(resp => resp.json())
-          .then((weather) => {
-            console.log(weather);
-            updateWeatherCard(weather);
-          })
-
+//Obtener latitude & longitude con la api de navigator.geolocation
+(function getGeolocation() {
+    console.log(navigator.geolocation)
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (data) => {
+                console.log(data);
+                latitude = data.coords.latitude;
+                longitude = data.coords.longitude;
+                searchWeatherCity();
+            },
+            (error) => {
+                console.log(error)
+            }
+        )
+    }
+})()
 
 function updateWeatherCard(weather) {
     const tempHTML = document.getElementById('weather-temp')
@@ -18,5 +31,25 @@ function updateWeatherCard(weather) {
 
     nameHTML.innerText = weather.name;
     tempHTML.innerHTML = `${weather.main.temp} <span>°C</span>`
-    iconHTML.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}.png" />`
+    iconHTML.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" />`
+}
+
+//Listener del evento de presionar teclas, esperando que la key sea enter para llamar a la función search city
+weatherInput.addEventListener('keyup', (evt) => evt.key === 'Enter' ?  searchWeatherCity(evt.target.value) : null);
+
+
+//
+function searchWeatherCity(city) {
+    const params = city ? `&q=${city}` : `&lat=${latitude}&lon=${longitude}`
+    fetch(`${URL}${params}`).then(resp => resp.json())
+          .then((weather) => {
+            console.log(weather);
+            if(weather.cod === '404') swal('Error', 'Ciudad no encontrada', 'error')
+            updateWeatherCard(weather);
+          })
+}
+
+function searchWeatherCityBtn() {
+    const cityValue = weatherInput.value;
+    searchWeatherCity(cityValue)
 }
